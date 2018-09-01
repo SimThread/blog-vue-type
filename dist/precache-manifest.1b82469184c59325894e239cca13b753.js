026db9ca92307e411aa3222f8facebfffb4e9532
SimpleThread <simplethread@163.com>
2018-09-01 11:21:37 +0800

    cx: 提交打包文件

---
+++ dist/precache-manifest.1b82469184c59325894e239cca13b753.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/img/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/fonts/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "5d32390bd0afc2d2eae1",
+    "url": "/js/61c2.07e18b9b.js"
+  },
+  {
+    "revision": "13d6785907c5896acfcd",
+    "url": "/js/app.3184a8ab.js"
+  },
+  {
+    "revision": "62d629295cde3fb76a89",
+    "url": "/js/chunk-3b9c.9bbc69f5.js"
+  },
+  {
+    "revision": "04606402d25f981f607f",
+    "url": "/js/chunk-9027.04b969f7.js"
+  },
+  {
+    "revision": "27ef3249f57532acc16d",
+    "url": "/js/chunk-b849.d6fe20a4.js"
+  },
+  {
+    "revision": "db42858e0ca2a1608a87",
+    "url": "/js/7f2e.022a1bf2.js"
+  },
+  {
+    "revision": "6beef58e57a7888c01fc",
+    "url": "/js/chunk-vendors.c027ba03.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/fonts/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "45f459dd3cebc6a0bed2bc150be47df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/fonts/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/fonts/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "6beef58e57a7888c01fc",
+    "url": "/css/chunk-vendors.c001354f.css"
+  },
+  {
+    "revision": "62d629295cde3fb76a89",
+    "url": "/css/chunk-3b9c.d2b4e4d8.css"
+  },
+  {
+    "revision": "13d6785907c5896acfcd",
+    "url": "/css/app.c63ecaa0.css"
+  }
+];
